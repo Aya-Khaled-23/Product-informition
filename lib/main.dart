cc7d3133f0c0932c61bfa4f7d4b271cde3d6b2ac
Aya-Khaled-23 <ayakhaled814@gmail.com>
2020-03-19 17:55:29 +0200

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:productsdetails/productsInfo.dart';
+main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: productsInfo(),
+  ));
+}
